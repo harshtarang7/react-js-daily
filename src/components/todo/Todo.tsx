938a1847fdbe6a0a17ad99c1f566362478de2781
harshtarang7 <tarangharsh7@gmail.com>
2025-07-20 23:11:28 +0530

    cleaned todo app

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -9,8 +9,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+
 import { useTodo } from "../../customHook/Todo";
 
 export interface task {
@@ -20,12 +19,9 @@ export interface task {
   priority?: "high" | "medium" | "low" ;
 }
 export const Todo = () => {
-  // const [inputTask, setInputTask] = useState("");
-  // const [priority, setPriority] = useState<"high" | "medium" | "low">("low");
-  // const [savedTask, setSavedTask] = useState<task[]>([]);
 
   const {
-     inputTask,
+    inputTask,
     priority,
     savedTask,
     handleAddTask,
@@ -37,72 +33,6 @@ export const Todo = () => {
     setPriority,
   } = useTodo();
 
-  // const handleAddTask = () => {
-  //   if (inputTask.trim() !== "") {
-  //     const newTask: task = {
-  //       id: uuidv4(),
-  //       task: inputTask,
-  //       checked: false,
-  //       priority: priority,
-  //     };
-  //     const newSavedTask = [...savedTask, newTask];
-  //     localStorage.setItem("task", JSON.stringify(newSavedTask));
-  //     setSavedTask(newSavedTask);
-  //     setInputTask("");
-  //     setPriority("low");
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   const savedTasks = localStorage.getItem("task");
-  //   if (savedTasks) {
-  //     const parsedTasks = JSON.parse(savedTasks);
-  //     const taskWithPriority = parsedTasks.map((task:any)=>({
-  //       ...task,
-  //       priority:task.priority || 'low'
-  //     }))
-  //     setSavedTask(taskWithPriority)
-  //   }
-  // }, []);
-
-  // const handleChecked = (id: string) => {
-  //   const updatedTasks = savedTask.map((task) =>
-  //     task.id === id ? { ...task, checked: !task.checked } : task
-  //   );
-  //   setSavedTask(updatedTasks);
-  //   localStorage.setItem("task", JSON.stringify(updatedTasks));
-  // };
-
-  // const handlePrioriy = (
-  //   id: string,
-  //   newPriority: "high" | "medium" | "low"
-  // ) => {
-
-  //   const updatedTasks = savedTask.map((task) =>
-  //     task.id === id ? { ...task, priority: newPriority } : task
-  //   );
-  //   setSavedTask(updatedTasks);
-  //   localStorage.setItem("task", JSON.stringify(updatedTasks));
-  // };
-
-  // const handleDelete = (id: string) => {
-  //   const updatedTasks = savedTask.filter((task) => task.id !== id);
-  //   setSavedTask(updatedTasks);
-  //   localStorage.setItem("task", JSON.stringify(updatedTasks));
-  // };
-
-  // const getPriorityColor = (priority: "high" | "medium" | "low") => {
-  //   switch (priority) {
-  //     case "high":
-  //       return "#ff4444";
-  //     case "medium":
-  //       return "#ff9900";
-  //     case "low":
-  //       return "#00aa00";
-  //     default:
-  //       return "#666666";
-  //   }
-  // };
   return (
     <Box
       sx={{
